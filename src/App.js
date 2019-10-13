7a30c80b594315abfda5c6e949404945d00f5f1c
Ricardo <568082704@qq.com>
2019-10-13 16:37:57 +0800

    save

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import { useState } from "react"
 
 function App() {
+  const [count/*值*/ ,setCount/*更新函数 */] = useState(0) //解构赋值
+  const add=()=>{
+    setCount(count+1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{count}</div>
+      <div>
+        <button onClick={add}>+1</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
